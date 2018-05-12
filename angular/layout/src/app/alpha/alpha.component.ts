d7f27ce61795adba5e993457c29f357d611493ac
Shannon Steen <Steen.ShannonM@gmail.com>
2018-05-12 11:58:26 -0400

    layout missing components

---
+++ angular/layout/src/app/alpha/alpha.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alpha',
+  templateUrl: './alpha.component.html',
+  styleUrls: ['./alpha.component.css']
+})
+export class AlphaComponent implements OnInit {
+  alphaCompData = "This represents the AlphaComponent Data.";
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
